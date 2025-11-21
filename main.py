f0fbe7d6066d1245e3f0f07363aa5809ce30ab35
jacobrluttrull <jacobrluttrull@gmail.com>
2025-11-21 17:39:49 -0500

    made stats.py and moved get_num_words

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(book_path):
+    pass
+def main():
+    book_path = "books/frankenstein.txt"
+    with open(book_path) as f:
+        book_text = f.read()
+        #print(book_text)
+        print(get_num_words(book_text))
+
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(book_text):
+    split_text = book_text.split()
+    return f"Found {len(split_text)} total words"
